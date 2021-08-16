fff1e67d5805b279e78032ddacc610ffbd3fcabd
Paulinafridman <fridman.pauliasofia@gmail.com>
2021-08-16 11:04:42 -0300

    fox final

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const foxcontainer =
 document.querySelector("#imgcontainer");
 foximage = async () => {
-    fetch("http://randomfox.ca/floof/")
+    fetch("https://randomfox.ca/floof/")
         .then(res => res.json())
         .then(data => {
             foxcontainer.src = data.image;
         })
         .catch(()=>{
-            document.querySelector(".error").innerHTML = "Problem loading data(problemas al cargar)";
+            document.querySelector(".error").innerHTML = 
+            "Problem loading data(problemas al cargar)";
         });
 }
 foximage();
